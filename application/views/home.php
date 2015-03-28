334a46af2c4a41d3a9990a0327f84efde79f3acd
Reics <reics.1.1@gmail.com>
2015-03-28 14:58:31 -0600

    initial commit Codeigniter 2 + Bootstrap 3

---
+++ application/views/home.php
@@ -0,0 +1,10 @@
+<section>
+  <div class="container">
+    <h1>Hello World!</h1>
+    <p class="lead">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc felis nunc, tempus eget tortor nec, convallis volutpat lacus. Curabitur id rhoncus sapien. Sed in sem non mi sollicitudin cursus. Proin ultricies, velit ut malesuada euismod, sapien felis rhoncus purus, a condimentum nisl lorem sit amet sem. Integer vitae risus laoreet, luctus justo id, molestie orci. Nam ipsum erat, imperdiet ac convallis et, luctus ac massa. Mauris et tellus non ex lobortis tempus sed sit amet diam. Nunc sollicitudin lorem leo, id sollicitudin eros ultrices vel. In faucibus arcu sed orci consectetur dignissim. Sed augue mauris, vehicula vitae nulla sed, lobortis hendrerit tellus. Etiam sagittis tellus vel arcu convallis dignissim. Cras a augue ornare, scelerisque diam quis, iaculis elit. Quisque malesuada orci non risus porta, vel facilisis quam ultrices. Suspendisse feugiat felis eget aliquam viverra.
+
+Suspendisse et velit vel mi scelerisque fermentum. Donec condimentum consectetur consequat. Nulla facilisi. Vestibulum vel lectus mi. Ut quis aliquam nisi, non pretium risus. Etiam massa ipsum, eleifend eu pretium sed, condimentum eu orci. Phasellus vitae urna nulla. Praesent sed purus venenatis, cursus est sit amet, aliquam sem. Morbi a est finibus, ultrices velit in, imperdiet est.
+    </p>
+  </div>
+</section>
