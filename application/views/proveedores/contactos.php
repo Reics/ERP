9348dba3109eb736286f8ab2b81315b648c97874
SunnySup <sonia.yo.x@gmail.com>
2015-04-09 14:43:43 -0400

    proveedores fashion

--- application/views/proveedores/contactos.php
+++ application/views/proveedores/contactos.php
@@ -1,71 +1,73 @@
 
-    <section style="margin-left: 50px; margin-right: 50px;">
-<div class="col-sm-offset-1">
-<div class="form-group">
-<br>
-<blockquote>
-  <p>Contacto</p>
-</blockquote>
-<form class="form-inline">
-  <div class="form-group">
-    <label for="ncontacto" style="
-    margin-bottom: 25px;
-">Nombre del Contacto: </label>
-    <input type="text" class="form-control" id="ncontacto">
-  </div>
-  </form>
-  <form class="form-inline">
-  <div class="form-group">
-    <label for="ccontacto" style="
-    margin-left: 6px;
-    margin-bottom: 25px;
-">Codigo del Contacto: </label>
-    <input type="text" class="form-control" id="ccontacto">
-  </div>
-  </form>
-  <form class="form-inline">
-  <div class="form-group">
-    <label for="direccion" style="
-    margin-left: 80px;
-    margin-bottom: 25px;
-">Direccion: </label>
-    <input type="text" class="form-control" id="direccion">
-  </div>
-  </form>
-   <form class="form-inline">
-  <div class="form-group">
-    <label for="rfc" style="
-    margin-left: 115px;
-    margin-bottom: 25px;
-">RFC: </label>
-    <input type="text" class="form-control" id="rfc">
-    </div>
-    <div class="form-group"> <label for:"estados"="" style="
-    margin-left: 10px;
-">Provincia: </label>
-<select class="form-control" name="estados">
-</select>
-  </div>
-  </form>
-    <form class="form-inline">
-  <div class="form-group">
-    <label for="telefono" style="
-    margin-left: 90px;
-    margin-bottom: 25px;
-">Telefono: </label>
-    <input type="text" class="form-control" id="telefono">
-  </div>
-    <div class="form-group">
-    <label for="email" style="
-    margin-left: 30px;
-">Email: </label>
-    <input type="text" class="form-control" id="email">
-  </div>
-  </form>
-  <div class="col-sm-offset-8 col-sm-10">
-<button type="submit" class="btn btn-success">Guardar</button>
-<button type="submit" class="btn btn-danger">Cancelar</button>
+<section style="margin-left: 50px; margin-right: 50px;">
+
+
+<div class="row">
+
+<legend align="center">Proveedor</legend>
+<div>
+<a role="button" class="btn btn-primary btn-lg" href="<?php echo site_url("proveedores/listado/datos");?>">Ficha</a>
+<a role="button" class="btn btn-primary btn-lg" href="<?php echo site_url("proveedores/listado/mapa");?>">Mapa</a>
+<a role="button" class="btn btn-primary btn-lg" href="<?php echo site_url("proveedores/listado/contactos");?>">Contacto</a>
+</div> 
+
+<legend align="center">Contacto</legend>
+<div align="center">
+                <table style="margin-left: 50px; margin left:30px">
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="ncontacto">Nombre del Contacto: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="ncontacto">                       
+                        </td>
+                    </div>
+                </form>
+            </tr>
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="ccontacto">Codigo del Contacto: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="ccontacto"></td>
+                    </div>
+                </form>
+             </tr>  
+             <tr> 
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td> <label for="direccion">Direccion: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="direccion"></td>
+                    </div>
+                </form>
+            </tr>
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td> <label for="rfc">RFC: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="rfc"></td>
+                        <td> <label for="rfc">Provincia: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="provincia"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="telefono">Telefono: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="telefono"></td>
+                        <td> <label for="email">Email: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="email"></td>
+                    </div>
+                </form>
+                </tr>
+                
+                
+</table>
 </div>
+                <div class="col-sm-offset-8 col-sm-10">
+                                        <a role="button" class="btn btn-success" href="<?php echo site_url("proveedores/listado/aecontacto");?>">Editar</a>
+                </div>
+            </div>
+
 </div>
-</div>
-    </section>
+
+</section>
--- application/views/proveedores/datos.php
+++ application/views/proveedores/datos.php
@@ -1,113 +1,145 @@
 
-<section class="container">
+<section style="margin-left: 50px; margin-right: 50px;">
 
-  <form class="form-inline">
-  <div class="form-group">
-    <label for="idProvador">ID: </label>
-    <input type="text" class="form-control" id="idProvador">
-  </div>
-  </form>
-  <form class="form-inline">
-  <div class="form-group">
-    <label for="Nombre">Nombre: </label>
-    <input type="text" class="form-control" id="Nombre">
-  </div>
-  </form>
-  <form class="form-inline">
-  <div class="form-group">
-    <label for="Empresa">Empresa: </label>
-    <input type="text" class="form-control" id="Empresa">
-  </div>
-  </form>
-    <form class="form-inline">
-  <div class="form-group">
-    <label for="titulodecortesia">Titulo de Cortesia: </label>
-    <input type="text" class="form-control" id="titulodecortesia">
-  </div>
-  </form>
-<form class="form-inline">
-  <div class="form-group">
-    <label for="puestofuncion">Puesto Función: </label>
-    <input type="text" class="form-control" id="puestofuncion">
-  </div>
-  </form>
-<form class="form-inline">
-  <div class="form-group">
-    <label for="Direccion">Dirección: </label>
-    <input type="text" class="form-control" id="Direccion">
-  </div>
-  </form>
-<form class="form-inline">
-    <div class="form-group">
-        <label for="codigopostal">Codigo Postal: </label>
-        <input type="text" class="form-control" id="cosigopostal">
-    </div>
-    <div class="form-group">
-        <label for="Poblacion">Población: </label>
-        <input type="text" class="form-control" id="Poblacion">
-    </div>
-  </form>
-<form class="form-inline">
-  <div class="form-group">
-    <label for="Pais">Pais: </label>
-    <input type="text" class="form-control" id="Pais">
-  </div>
-  </form>
-<form class="form-inline">
-  <div class="form-group">
-    <label for="Provincia">Provincia: </label>
-    <input type="text" class="form-control" id="Provincia">
-  </div>
-  </form>
-<form class="form-inline">
-    <div class="form-group">
-        <label for="numero1">Telefono Trabajo: </label>
-        <input type="text" class="form-control" id="numero1">
-    </div>
-    <div class="form-group">
-        <label for="numero2">Telefono Particular: </label>
-        <input type="text" class="form-control" id="numero2">
-    </div>
-  </form>
-<form class="form-inline">
-  <div class="form-group">
-    <label for="numero3">Movil: </label>
-    <input type="text" class="form-control" id="numero3">
-  </div>
-  </form>
-<form class="form-inline">
-    <div class="form-group">
-        <label for="correo1">Email 1: </label>
-        <input type="text" class="form-control" id="correo1">
-    </div>
-    <div class="form-group">
-        <label for="correo2">Email 2: </label>
-        <input type="text" class="form-control" id="correo2">
-    </div>
-  </form>
-<form class="form-inline">
-    <div class="form-group">
-        <label for="Twiter1">Red Social 1: </label>
-        <input type="text" class="form-control" id="Web">
-    </div>
-    <div class="form-group">
-        <label for="Facebook1">Red Social 2: </label>
-        <input type="text" class="form-control" id="Web">
-    </div>
-  </form>
 
-  <div class="col-sm-offset-8 col-sm-10">
-  <a type='button' class='btn btn-primary' aria-label='Left Align' href="<?php echo site_url("proveedores/listado/contacto");?>">
-      Editar
-   </a>
+<div class="row">
 
-   <a type='button' class='btn btn-primary' aria-label='Left Align' href="<?php echo site_url("proveedores/listado/contactos");?>">
-      Contacto
-   </a>
+<legend align="center">Proveedor</legend>
+<div>
+<a role="button" class="btn btn-primary btn-lg" href="<?php echo site_url("proveedores/listado/datos");?>">Ficha</a>
+<a role="button" class="btn btn-primary btn-lg" href="<?php echo site_url("proveedores/listado/mapa");?>">Mapa</a>
+<a role="button" class="btn btn-primary btn-lg" href="<?php echo site_url("proveedores/listado/contactos");?>">Contacto</a>
+</div> 
 
-   <a type='button' class='btn btn-primary' aria-label='Left Align' href="<?php echo site_url("proveedores/listado/mapa");?>">
-      Mapa
-   </a>
+<legend align="center">Fichas</legend>
+<div align="center">
+                <table style="margin-left: 50px; margin left:30px">
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="idProvador">ID: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="idProvador"></td>
+                    </div>
+                </form>
+            </tr>
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="Nombre">Nombre: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Nombre"></td>
+                    </div>
+                </form>
+             </tr>  
+             <tr> 
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="Empresa">Empresa: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Empresa"></td>
+                    </div>
+                </form>
+            </tr>
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="titulodecortesia">Titulo de Cortesia: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="titulodecortesia"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="puestofuncion">Puesto Funcion: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="puestofuncion"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="Direccion">Direccion: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Direccion"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="codigopostal">Codigo Postal: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="cosigopostal"></td>
+                    </div>
+                    <div class="form-group">
+                        <td><label for="Poblacion">Poblacion: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Poblacion"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="Pais">Pais: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Pais"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="Provincia">Provincia: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Provincia"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="numero1">Telefono Trabajo: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="numero1"></td>
+                    </div>
+                    <div class="form-group">
+                        <td><label for="numero2">Telefono Particular: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="numero2"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="numero3">Movil: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="numero3"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="correo1">Email 1: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="correo1"></td>
+                    </div>
+                    <div class="form-group">
+                        <td><label for="correo2">Email 2: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="correo2"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="Twiter1">Red Social 1: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Web"></td>
+                    </div>
+                    <div class="form-group">
+                        <td><label for="Facebook1">Red Social 2: </label></td>
+                        <td><input type="text" readonly="readonly" class="form-control" id="Web"></td>
+                    </div>
+                </form>
+                </tr>
+</table>
+</div>
+                <div class="col-sm-offset-8 col-sm-10">
+                                        <a role="button" class="btn btn-success" href="<?php echo site_url("proveedores/contacto");?>">Editar</a>
+                </div>
+            </div>
 
 </div>
 
