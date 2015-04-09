b5dbbd9f852f38e92a4e7cfe6ca99f94c733a0b9
SunnySup <sonia.yo.x@gmail.com>
2015-04-09 15:10:08 -0400

    fashion

---
+++ application/views/proveedores/agregareditarcontacto.php
@@ -0,0 +1,59 @@
+
+<section>
+  <div class="form-group">
+  <table style="margin-left: 50px; margin left:30px">
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="ncontacto">Nombre del Contacto: </label></td>
+                        <td><input type="text" class="form-control" class="form-control" id="ncontacto">                       
+                        </td>
+                    </div>
+                </form>
+            </tr>
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="ccontacto">Codigo del Contacto: </label></td>
+                        <td><input type="text" class="form-control" class="form-control" id="ccontacto"></td>
+                    </div>
+                </form>
+             </tr>  
+             <tr> 
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td> <label for="direccion">Direccion: </label></td>
+                        <td><input type="text" class="form-control" class="form-control" id="direccion"></td>
+                    </div>
+                </form>
+            </tr>
+            <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td> <label for="rfc">RFC: </label></td>
+                        <td><input type="text" class="form-control" class="form-control" id="rfc"></td>
+                        <td> <label for="rfc">Provincia: </label></td>
+                        <td><input type="text" class="form-control" class="form-control" id="provincia"></td>
+                    </div>
+                </form>
+                </tr>
+                <tr>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <td><label for="telefono">Telefono: </label></td>
+                        <td><input type="text" class="form-control" class="form-control" id="telefono"></td>
+                        <td> <label for="email">Email: </label></td>
+                        <td><input type="text" class="form-control" class="form-control" id="email"></td>
+                    </div>
+                </form>
+                </tr>
+                
+                
+</table>
+  <div class="col-sm-offset-8 col-sm-10">
+<button type="submit" class="btn btn-success">Guardar</button>
+<button type="submit" class="btn btn-danger">Cancelar</button>
+</div>
+</div>
+
+</section>
