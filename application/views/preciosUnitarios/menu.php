fb6920b77a9c249f35d5509e80634b77e3cfe6b0
SunnySup <sonia.yo.x@gmail.com>
2015-04-10 00:09:19 -0500

    Creación de modelos productos y productosPresupuestos

--- application/views/preciosUnitarios/menu.php
+++ application/views/preciosUnitarios/menu.php
@@ -3,8 +3,8 @@
   <div align="center" style="margin-left:150px;">
     <table width="879" height="188" border="0" style="margin-top:50px; margin-left:220px;">
       <tr>
-        <td align="center" width="200" height="150"><p align="center"><img src="<?php echo base_url("assets/img/preciosUnitarios/productos.jpg");?>" width="200" height="150"  alt=""/></p></td>
-        <td align="center" width="200" height="150"><p align="center"><img src="<?php echo base_url("assets/img/preciosUnitarios/presupuestos.jpg");?>" width="200" height="150"  alt=""/></p></td>
+        <td align="center" width="200" height="150"><p align="center"><a href="<?php echo site_url("preciosUnitarios/productos");?>"><img src="<?php echo base_url("assets/img/preciosUnitarios/productos.jpg");?>" width="200" height="150"  alt=""/></a></p></td>
+        <td align="center" width="200" height="150"><p align="center"><a href="<?php echo site_url("preciosUnitarios/presupuestos");?>"><img src="<?php echo base_url("assets/img/preciosUnitarios/presupuestos.jpg");?>" width="200" height="150"  alt=""/></a></p></td>
       </tr>
       <tr>
         <th> <h3 align="center">Productos</h4></th>
--- application/views/preciosUnitarios/productos/agregarProducto.php
+++ application/views/preciosUnitarios/productos/agregarProducto.php
@@ -1,57 +1,48 @@
 <section>
-    <div class="col-md-8">
-    </div>
-      <div class="col-md-2">
-          <input id="Search" name="Search" type="text" placeholder="Search..." class="form-control input-md">
-        </div> 
+    
+<div class="col-md-8">
+</div>
 
+<div class="col-md-2">
+  <input id="Search" name="Search" type="text" placeholder="Search..." class="form-control input-md" style="margin-top:20px;">
+</div> 
+
+<legend style="margin-top:20px; padding-top: 30px; margin-bottom: 40px;" align="center">Agregar Producto</legend>
+
+<div style="margin-left: 400px; width: 900px;" aling="center">
     <form class="form-horizontal">
-
-    <legend style="color:#FFFFFF">Agregar producto</legend>
-
-      <div class="form-group">
-        <label class="col-md-2 control-label etiquetas" for="Nombre">Nombre</label>  
-        <div class="col-md-8">
-          <input id="Nombre" name="Nombre" type="text" placeholder="Nombre del producto" class="form-control input-md" required="">
-        </div>  
+      <div class="form-group col-md-8">
+        <label class="control-label etiquetas" for="Nombre" style="padding-left: 0px; padding-right: 0px;">Nombre</label>  
+        <input id="Nombre" name="Nombre" type="text" placeholder="Nombre del producto" class="form-control input-md" required="">
       </div>
 
-      <div class="form-group">
-        <label class="col-md-2 control-label etiquetas" for="Unidad">Unidad</label>
-        <div class="col-md-2">
+      <div class="form-group col-md-8">
+        <label class="control-label etiquetas" for="Unidad" style="padding-left: 0px; padding-right: 0px;">Unidad</label>
           <select id="Unidad" name="Unidad" class="form-control">
             <option value="1">Option one</option>
             <option value="2">Option two</option>
           </select>
-        </div>
       </div>
 
-      <div class="form-group">
-        <label class="col-md-2 control-label etiquetas" for="Precio unitario">Precio unitario  $</label>  
-        <div class="col-md-2">
-          <input id="Precio unitario" name="Precio unitario" type="text" placeholder="Precio" class="form-control input-md" required="">
-        </div>  
+      <div class="form-group col-md-8">
+        <label class="col-md-2 control-label etiquetas" for="Precio unitario" style="width: 150px; padding-left: 0px; padding-right: 0px;">Precio unitario  $</label>  
+        <input id="Precio unitario" name="Precio unitario" type="text" placeholder="Precio" class="form-control input-md" required="">
       </div>
 
-      <div class="form-group">
-        <label class="col-md-2 control-label etiquetas" for="Categoria">Categoria</label>
-        <div class="col-md-2">
-          <select id="Categoria" name="Categoria" class="form-control">
-            <option value="1">Option one</option>
-            <option value="2">Option two</option>
-          </select>
-        </div>
+      <div class="form-group col-md-8">
+        <label class="col-md-2 control-label etiquetas" for="Categoria" style="padding-left: 0px; padding-right: 0px;">Categoria</label>
+        <select id="Categoria" name="Categoria" class="form-control">
+          <option value="1">Option one</option>
+          <option value="2">Option two</option>
+        </select>
       </div>
-
-      <div class="row" style="margin-top: 50px; margin-left: 175px;" >
-        <div class="col-md-4" style="margin-: 50px">
-        <button id="Guardar" name="Guardar" class="btn btn-success">Guardar</button>
-      </div>
-      <div class="col-md-4" style="margin-left: 330px;">
-        <button id="Cancelar" name="Cancelar" class="btn btn-danger">Cancelar</button>
+    </form>
+    <div class="col-md-8" style="margin-left:100px;">
+      <div class="row">
+        <button id="Guardar" name="Guardar" class="btn btn-success" >Guardar</button>
+        <button id="Cancelar" name="Cancelar" class="btn btn-danger" style=" margin-left: 175px;">Cancelar</button>
       </div>
     </div>
-
-    </form>
+</div>
 
 </section>
