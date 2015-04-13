0c849076562f18f4f2a2f43faa7e09cc500b5f3d
Reics <reics.1.1@gmail.com>
2015-04-13 00:16:41 -0500

    profiler down

--- application/controllers/proveedores/listado.php
+++ application/controllers/proveedores/listado.php
@@ -6,8 +6,7 @@ class Listado extends CI_Controller {
 	       public function __construct() 
         { 
             parent::__construct();
-            $this->load->helper('url'); 
-             $this->output->enable_profiler(TRUE);
+            $this->load->helper('url');
         }
 
 
